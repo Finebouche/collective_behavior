222d4d836aca8b72ea872a17092c856d8ad95f04
Finebouche <tanguy.cazalets@protonmail.com>
2024-08-28 16:20:56 +0000

    update to analysis

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ run_config = dict(
         use_vectorized=False,   # mostly useless for low number of fish
         step_per_time_increment = 5, # int ≥ 1
         # General
-        num_preys=20,
-        num_predators=5,
-        stage_size=20,
+        num_preys=30,
+        num_predators=2,
+        stage_size=30,
         episode_length=700,
         prey_consumed=True,
         # Physics
         inertia=True,
-        dragging_force_coefficient=0.3,
+        dragging_force_coefficient=2,
         contact_force_coefficient=5,
         contact_margin=0.2,
-        friction_regime="intermediate",            # linear=1, quadratic=2 or intermediate=1.4
+        friction_regime="linear",            # linear=1, quadratic=2 or intermediate=1.4
         periodical_boundary=False,           # If False, the wall is solid
-        wall_contact_force_coefficient=10,    # Only used when periodical_boundary=False, else ignored
+        wall_contact_force_coefficient=5,    # Only used when periodical_boundary=False, else ignored
         prey_radius=0.1,
         predator_radius=0.4,
         agent_density=1000,  # density of the agents to calculate the mass (should be arround 1000 if radius about 0.1)
@@ -38,7 +38,7 @@ run_config = dict(
         death_penalty_for_prey=-1.0,
         collective_death_penalty_for_prey=-0,
         edge_hit_penalty=-0.01,
-        energy_cost_penalty_coef=0, # positive < 1 (this is for a ratio)
+        energy_cost_penalty_coef=0.001, # positive < 1 (this is for a ratio)
         # Observations
         max_seeing_angle=3*np.pi/4,  # Put None if not used (between 0 and pi)
         max_seeing_distance=20,  # Put None if not used
